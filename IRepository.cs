54d56ef86c23c788a3cd10e70ea3addda776b15b
CourtneySimone <57356890+CourtneySimone@users.noreply.github.com>
2019-11-04 15:28:23 +0200

    Add files via upload

---
+++ IRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+   public interface IRepository<T> where T : class
+
+    {
+        List<T> GetAll();
+
+        T FindById(int Id);
+
+        void Add(T entity);
+
+        void Delete(T entity);
+
+        void Edit(T entity);
+
+        void Save();
+
+    }
+}
---
+++ Repository.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using System.Data;
+
+namespace Repository
+{
+    //This is my Generic Repo
+    public class Repository<C, T> :
+   IRepository<T> where T : class where C : DbContext, new()
+    {
+
+        private C entity = new C();
+        public C Context
+        {
+
+            get { return entity; }
+            set { entity = value; }
+        }
+
+        public virtual List<T> GetAll()
+        {
+            return entity.Set<T>().AsEnumerable().ToList();
+        }
+
+        public T FindById(int Id)
+        {
+            return entity.Set<T>().Find(Id);
+        }
+
+        public virtual void Add(T entity)
+        {
+            this.entity.Set<T>().Add(entity);
+        }
+
+        public virtual void Delete(T entity)
+        {
+            this.entity.Set<T>().Remove(entity);
+        }
+
+        public virtual void Edit(T entity)
+        {
+            this.entity.Entry(entity).State = EntityState.Modified;
+        }
+
+        public virtual void Save()
+        {
+            entity.SaveChanges();
+        }
+    }
+}
